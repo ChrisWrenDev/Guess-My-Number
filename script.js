489906e9a5f5d9a35b8611658c00fd872321f393
Chris Wren <dazedpandas@gmail.com>
2021-03-23 11:32:31 +0000

    Generate random number and compare user guess

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-'use strict';
+"use strict";
+
+//Generate Random Secret Number
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+document.querySelector(".number").textContent = secretNumber;
+
+//Comapre Users Guess to Secret Number
+document.querySelector(".check").addEventListener("click", function () {
+  const guess = Number(document.querySelector(".guess").value);
+
+  if (!guess) {
+    document.querySelector(".message").textContent = "No number!";
+  } else if (guess === secretNumber) {
+    document.querySelector(".message").textContent = "Correct Number!";
+  } else if (guess > secretNumber) {
+    document.querySelector(".message").textContent = "Too High!";
+  } else if (guess < secretNumber) {
+    document.querySelector(".message").textContent = "Too Low!";
+  }
+});
